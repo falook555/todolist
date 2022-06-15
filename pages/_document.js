225b208ed5c2f0cfc632a20ccbabd22d3095532b
falook555 <falook555@gmail.com>
2022-06-15 15:32:04 +0700

    sadasd

--- pages/_document.js
+++ pages/_document.js
@@ -20,12 +20,12 @@ class MyDocument extends Document {
                         <link rel="stylesheet" href="https://cdn-uicons.flaticon.com/uicons-regular-rounded/css/uicons-regular-rounded.css" />
 
                         <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css" />
-                        <link rel="stylesheet" href="http://localhost:3000/static/plugins/fontawesome-free/css/all.min.css" />
-                        <link rel="stylesheet" href="http://localhost:3000/static/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css" />
-                        <link rel="stylesheet" href="http://localhost:3000/static/plugins/icheck-bootstrap/icheck-bootstrap.min.css" />
-                        <link rel="stylesheet" href="http://localhost:3000/static/dist/css/adminlte.min.css" />
-                        <link rel="stylesheet" href="http://localhost:3000/static/plugins/overlayScrollbars/css/OverlayScrollbars.min.css" />
-                        <link rel="stylesheet" href="http://localhost:3000/static/plugins/pace-progress/themes/blue/pace-theme-flat-top.css" />
+                        <link rel="stylesheet" href="static/plugins/fontawesome-free/css/all.min.css" />
+                        <link rel="stylesheet" href="static/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css" />
+                        <link rel="stylesheet" href="static/plugins/icheck-bootstrap/icheck-bootstrap.min.css" />
+                        <link rel="stylesheet" href="static/dist/css/adminlte.min.css" />
+                        <link rel="stylesheet" href="static/plugins/overlayScrollbars/css/OverlayScrollbars.min.css" />
+                        <link rel="stylesheet" href="static/plugins/pace-progress/themes/blue/pace-theme-flat-top.css" />
                     </div>
 
 
@@ -37,12 +37,9 @@ class MyDocument extends Document {
                         <NextScript />
 
                     </div>
-
-
-
-                    <script defer src="http://localhost:3000/static/plugins/jquery/jquery.min.js"></script>
-                    <script defer src="http://localhost:3000/static/plugins/jquery-ui/jquery-ui.min.js"></script>
-                    <script defer src="http://localhost:3000/static/dist/js/adminlte.js"></script>
+                    <script defer src="static/plugins/jquery/jquery.min.js"></script>
+                    <script defer src="static/plugins/jquery-ui/jquery-ui.min.js"></script>
+                    <script defer src="static/dist/js/adminlte.js"></script>
                     <script defer src="static/plugins/pace-progress/pace.min.js" />
                 </body>
             </Html>
