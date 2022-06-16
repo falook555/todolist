8c277192a2eb02cc5c3bfb423576af9f04e72d72
falook555 <falook555@gmail.com>
2022-06-16 14:02:30 +0700

    asdasdasd

--- pages/index.js
+++ pages/index.js
@@ -45,13 +45,13 @@ export default function Home() {
   return (
     <div>
       <div className="card-group">
-        <div className='card mt-2 ml-2 mr-1 mb-2' style={{ height: '1070px', backgroundColor: 'antiquewhite' }}>
-          <h1 className='text-center mt-2'>
+        <div className='card mt-2 ml-2 mr-1 mb-2' style={{ height: '1065px', backgroundColor: 'blue', color: 'white' }}>
+          <h1 className='text-center mt-2 text-white'>
             <b>กฤษฎา อนันตะ</b>
           </h1>
           <table className="table table-striped h4">
             <thead>
-              <tr>
+              <tr style={{ backgroundColor: '#0019D1' }}>
                 <th>สถานที่แจ้ง</th>
                 <th>รายละเอียด</th>
                 <th>เวลาทำงาน</th>
@@ -62,21 +62,21 @@ export default function Home() {
                 // console.log(item8.td_upDt == null ? 'เวลาออก : ' + item8.td_insDt : 'เวลากลับ : ' + item8.td_upDt)
                 // item8.td_upDt == null ? item8.td_insDt : item8.td_upDt
                 return <tr key={i8}>
-                  <td>{item8.td_dept.substring(0, 25)}...</td>
-                  <td>{item8.td_case.substring(0, 25)}...</td>
-                  <td>{moment(item8.td_upDt == null ? item8.td_insDt : item8.td_upDt).format('H:mm:ss')} น.</td>
+                  <td style={{ backgroundColor: i8 % 2 == 1 ? '#0019D1' : '#001FFF' }}>{item8.td_dept.substring(0, 20)}...</td>
+                  <td style={{ backgroundColor: i8 % 2 == 1 ? '#0019D1' : '#001FFF' }}>{item8.td_case.substring(0, 25)}...</td>
+                  <td style={{ backgroundColor: item8.td_upDt == null ? 'red' : 'green', width: '130px' , textAlign:'center' }}>{moment(item8.td_upDt == null ? item8.td_insDt : item8.td_upDt).format('H:mm')} น.</td>
                 </tr>
               })}
             </tbody>
           </table>
         </div>
-        <div className='card mt-2 ml-1 mr-1 mb-2' style={{ height: '1070px', backgroundColor: 'lightgreen' }}>
-          <h1 className='text-center mt-2'>
+        <div className='card mt-2 ml-1 mr-1 mb-2' style={{ height: '1065px', backgroundColor: 'blue', color: 'white' }}>
+          <h1 className='text-center mt-2 text-white'>
             <b>สุจินต์ สุกกล้า</b>
           </h1>
           <table className="table table-striped h4">
             <thead>
-              <tr>
+              <tr style={{ backgroundColor: '#0019D1' }}>
                 <th>สถานที่แจ้ง</th>
                 <th>รายละเอียด</th>
                 <th>เวลาทำงาน</th>
@@ -85,21 +85,21 @@ export default function Home() {
             <tbody>
               {ict9.map((item9, i9) => {
                 return <tr key={i9}>
-                  <td>{item9.td_dept.substring(0, 25)}...</td>
-                  <td>{item9.td_case.substring(0, 25)}...</td>
-                  <td>{moment(item9.td_upDt == null ? item9.td_insDt : item9.td_upDt).format('H:mm:ss')} น.</td>
+                  <td style={{ backgroundColor: i9 % 2 == 1 ? '#0019D1' : '#001FFF' }}>{item9.td_dept.substring(0, 20)}...</td>
+                  <td style={{ backgroundColor: i9 % 2 == 1 ? '#0019D1' : '#001FFF' }}>{item9.td_case.substring(0, 25)}...</td>
+                  <td style={{ backgroundColor: item9.td_upDt == null ? 'red' : 'green', width: '130px' , textAlign:'center' }}>{moment(item9.td_upDt == null ? item9.td_insDt : item9.td_upDt).format('H:mm')} น.</td>
                 </tr>
               })}
             </tbody>
           </table>
         </div>
-        <div className='card mt-2 ml-1 mr-2 mb-2' style={{ height: '1070px', backgroundColor: 'lightpink' }}>
-          <h1 className='text-center mt-2'>
+        <div className='card mt-2 ml-1 mr-2 mb-2' style={{ height: '1065px', backgroundColor: 'blue', color: 'white' }}>
+          <h1 className='text-center mt-2 text-white'>
             <b>กนต์ธร โทนทรัพย์</b>
           </h1>
           <table className="table table-striped h4">
             <thead>
-              <tr>
+              <tr style={{ backgroundColor: '#0019D1' }}>
                 <th>สถานที่แจ้ง</th>
                 <th>รายละเอียด</th>
                 <th>เวลาทำงาน</th>
@@ -108,9 +108,9 @@ export default function Home() {
             <tbody>
               {ict13.map((item13, i13) => {
                 return <tr key={i13}>
-                  <td>{item13.td_dept.substring(0, 25)}...</td>
-                  <td>{item13.td_case.substring(0, 25)}...</td>
-                  <td>{moment(item13.td_upDt == null ? item13.td_insDt : item13.td_upDt).format('H:mm:ss')} น.</td>
+                  <td style={{ backgroundColor: i13 % 2 == 1 ? '#0019D1' : '#001FFF' }}>{item13.td_dept.substring(0, 20)}...</td>
+                  <td style={{ backgroundColor: i13 % 2 == 1 ? '#0019D1' : '#001FFF' }}>{item13.td_case.substring(0, 25)}...</td>
+                  <td style={{ backgroundColor: item13.td_upDt == null ? 'red' : 'green', width: '130px' , textAlign:'center' }}>{moment(item13.td_upDt == null ? item13.td_insDt : item13.td_upDt).format('H:mm')} น.</td>
                 </tr>
               })}
             </tbody>
